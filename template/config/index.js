444bfd443fc24deb0bc6cdfc567118de2b324cc9
diandi <diandi@in66.com>
2017-02-21 17:30:24 +0800

    add

--- template/config/index.js
+++ template/config/index.js
@@ -1,3 +1,12 @@
+
+export const build = {
+  entryGlobs: 'src/pages/**/entry.js',
+  entryTpl: 'tpl.pug',
+  entryRule: ['entry.js', 'tpl.pug'],
+  remUnit: 75,
+  bundleAnalyzerReport: false
+}
+
 export const env = {
   port: 8018,
   autoOpenBrowser: true,
@@ -15,12 +24,4 @@ export const env = {
       }
     }
   }
-}
-
-export const build = {
-  entryGlobs: 'src/pages/**/entry.js',
-  entryTpl: 'tpl.pug',
-  entryRule: ['entry.js', 'tpl.pug'],
-  remUnit: 75,
-  bundleAnalyzerReport: false
 }
