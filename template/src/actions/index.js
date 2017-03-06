38f191316930e4d070b85c6f5c02d3b78453a09c
diandi <diandi@in66.com>
2017-03-06 16:26:09 +0800

    fix upload

--- template/src/actions/index.js
+++ template/src/actions/index.js
@@ -1,10 +1,58 @@
-import {
-  buildFetch
-} from './build'
+import { buildGetParam, buildPostParam } from './build'
+/**
+ * 构建请求方法（每个请求每次只能执行一次）
+ * @param {Object} baseParam
+ * @param {String} urls 请求地址
+ * @param {function} error
+ */
+const FetchApi = (baseParam, urls, error = (err) => {
+  throw err
+}) => {
+  /**
+   * 如果是线上则访问 www.in66.com
+   */
+  let host = process.env.NODE_ENV === 'production' ? '//www.in66.com' : ''
+  /**
+   * 如果是qa访问 qa.in66.com
+   */
+  if (/^qa/.test(location.host)) {
+    host = '//qa.in66.com'
+  }
+  let urlObj = {}
+  Object.keys(urls).forEach(urlKey => {
+    urlObj[`${urlKey}`] = (param, type = 'get') => {
+      let url = `${host}${urls[`${urlKey}`]}`
+      let options = {
+        credentials: 'include'
+      }
+      if (type === 'get') {
+        url = buildGetParam(url, baseParam, param)
+      } else {
+        options.method = 'POST'
+        options.body = buildPostParam(baseParam, param)
+      }
+      if (urlObj[`${urlKey}`].loading) {
+        return {
+          then: () => {},
+          catch: () => {}
+        }
+      }
+      urlObj[`${urlKey}`].loading = true
+      return fetch(url, options).then(res => {
+        urlObj[`${urlKey}`].loading = false
+        return res
+      }, err => {
+        urlObj[`${urlKey}`].loading = false
+        throw err
+      }).then(res => res.json()).catch(error)
+    }
+  })
+  return urlObj
+}
 
 // 通用错误
-const error = () => {
-
+const error = error => {
+  throw error
 }
 
 // 通用参数
@@ -12,17 +60,10 @@ const baseParam = {
   promo_name: '{{ name }}'
 }
 
-var host = ''
-if (/^qa/.test(location.host)) {
-  host = '//qa.in66.com'
-} else if (process.env.NODE_ENV === 'production') {
-  host = '//www.in66.com'
-}
-
 /**
  *  请求地址
  *  URL.xxx({}).then
  */
-export const URL = buildFetch(baseParam, {
-  xxx: `${host}/test`
+export const URL = FetchApi(baseParam, {
+  xxx: `/test`
 }, error)
