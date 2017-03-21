af0bd5b64f22d23007f403d1dc00a6c69550f49d
maifei <maifei@in66.com>
2017-03-21 21:17:00 +0800

    deleted:    src/iconfig.js

--- template/src/iconfig.js
+++
@@ -1,4 +0,0 @@
-export const U_IN = '//www.in66.com'
-export const U_IN_QA = '//qa.in66.com'
-export const U_TRACK = '//stats1.jiuyan.info/onepiece/router.html'
-export const U_IN_WXSDK = '//www.in66.com/promo/commonapi/getweixinjssdkconfig'
