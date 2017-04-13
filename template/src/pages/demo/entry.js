c4190601ee68eb70ba2b58d84ae382210b509e1a
diandi <diandi@in66.com>
2017-04-13 10:19:29 +0800

    add vue filter

---
+++ template/src/pages/demo/entry.js
@@ -0,0 +1,28 @@
+import {
+  polyfill
+} from 'es6-promise'
+import Vue from 'vue'
+import App from './App.vue'
+import {
+  initPage
+} from 'units/common'
+import 'whatwg-fetch'
+import {
+  iAvatar
+} from 'i-ui'
+polyfill()
+
+Vue.use(iAvatar)
+initPage(Vue, {
+  share: {
+    shareTitle: 'shareTitle', // 分享标题
+    shareDesc: 'shareDesc', // 分享描述
+    shareLink: location.protocol + `//h5.in66.com/inpromo/2017/{{ name }}/index.html?_ig=share`,
+    shareImg: location.protocol + '//inimg02.jiuyan.info/in/2017/02/27/307746C7-A0AC-4D21-4D92-B480A77ADFA2.jpg',
+    shareTrack: 'h5_promo_{{ name }}_index_share'
+  }
+})
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
--- template/src/pages/index/App.vue
+++ template/src/pages/index/App.vue
@@ -2,24 +2,19 @@
   <div class="page">
     Welcome
     <iAvatar src="//inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg"></iAvatar>
-    {{#if_eq vuex "yes"}}
-    \{{$store.state.count }}
+    {{$store.state.count }}
     <button @click="increment">加一\{{$store.state.increTimes}}</button>
     <button @click="decrement">减一\{{$store.state.decreTimes}}</button>
     <div>
       userid:\{{$store.state.user.id}} <br>name:\{{$store.state.user.name}}
     </div>
-    {{/if_eq}}
   </div>
 </template>
 <script>
-  {{#if_eq vuex "yes"}}
   import {
     mapActions
   } from 'vuex'
-  {{/if_eq}}
   export default {
-    {{#if_eq vuex "yes"}}
     methods: mapActions([
       'increment',
       'decrement'
@@ -27,7 +22,6 @@
     created () {
       this.$store.dispatch('getUser')
     }
-    {{/if_eq}}
   }
 
 </script>
