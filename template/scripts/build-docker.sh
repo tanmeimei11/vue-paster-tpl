b18ccb76fbcaa89c0ff4c0bbbdbae240edb745e0
maifei <maifei@in66.com>
2017-04-19 14:23:27 +0800

    change image name from vue to inVue

--- template/scripts/build-docker.sh
+++ template/scripts/build-docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-IMAGE_NAME=vue:vue-paster-tpl
+IMAGE_NAME=inVue:vue-paster-tpl
 DOCKERFILE=Dockerfile
 
 if [ ! -f "$DOCKERFILE"  ]; then
   echo 'no Dockerfile found' 
+  exit(1)
 fi
 
-CONTAINER_IDS=`docker ps -a| grep $IMAGE_NAME | awk {'print $1'}` 
+CONTAINER_IDS=`docker ps -a | grep $IMAGE_NAME | awk {'print $1'}` 
 docker stop $CONTAINER_IDS
 #docker rm $CONTAINER_IDS
 docker rmi $IMAGE_NAME
