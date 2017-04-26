7655493d40cd834e2e078f4bdaa15c80ddf0b243
diandi <diandi@in66.com>
2017-04-26 15:02:45 +0800

    fix h5 to daymic host

--- template/src/pages/demo/entry.js
+++ template/src/pages/demo/entry.js
@@ -17,7 +17,7 @@ initPage(Vue, {
   share: {
     shareTitle: 'shareTitle', // 分享标题
     shareDesc: 'shareDesc', // 分享描述
-    shareLink: location.protocol + `//h5.in66.com/inpromo/2017/{{ name }}/index.html?_ig=share`,
+    shareLink: location.protocol + `//${location.host}/inpromo/2017/{{ name }}/index.html?_ig=share`,
     shareImg: location.protocol + '//inimg02.jiuyan.info/in/2017/02/27/307746C7-A0AC-4D21-4D92-B480A77ADFA2.jpg',
     shareTrack: 'h5_promo_{{ name }}_index_share'
   }
