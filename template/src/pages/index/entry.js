590e800cb5fa0545647892a99fc1cf604edc0e93
diandi <diandi@in66.com>
2017-06-13 10:19:28 +0800

    fix mixin css and common config

--- template/src/pages/index/entry.js
+++ template/src/pages/index/entry.js
@@ -4,25 +4,28 @@ import { iTrack } from 'i-ui'
 import InVue from 'iExtends/InVue.js'
 import 'iScss/base.scss'
 import App from './App.vue'
-import 'assets/libs/jweixin-1.0.0.js' // 微信分享的sdk
-import 'assets/libs/growingio.js' // 活动必须的growingio埋点,其他的不用
-import { initMethod } from 'units/common' // 设置全局vue方法,也可以不用
+import 'assets/libs/jweixin-1.0.0.js' 
+// 微信分享的sdk
+import 'assets/libs/growingio.js' 
+// 活动必须的growingio埋点,其他的不用
+import { initMethod, SHARE_CONFIG, TRACK_PREFIX } from 'units/common' 
+// 设置全局vue方法,也可以不用
 import store from 'stores'
 // import 'mocks'
 
 // 设置埋点的公共前缀
-window._trackPrefix = `h5_promo_{{ name }}_${(location.pathname.split('/').slice(-1)[0].replace(/.html$/, '') || 'index')}_`
+window._trackPrefix = TRACK_PREFIX
 
 new InVue({
   store,
   plugins: [iTrack, initMethod], // 类似vue.use
   track: 'enter', // 设置页面打开埋点
   share: {
-    shareTitle: 'shareTitle', // 分享标题
-    shareDesc: 'shareDesc', // 分享描述
-    shareLink: location.protocol + `//${location.host}/inpromo/2017/{{ name }}/index.html?_ig=share`,
-    shareImg: location.protocol + '//inimg02.jiuyan.info/in/2017/02/27/307746C7-A0AC-4D21-4D92-B480A77ADFA2.jpg',
-    shareTrack: 'share'
+    shareTitle: SHARE_CONFIG.shareTitle, // 分享标题
+    shareDesc: SHARE_CONFIG.shareDesc, // 分享描述
+    shareLink: SHARE_CONFIG.shareLink,
+    shareImg: SHARE_CONFIG.shareImg,
+    shareTrack: SHARE_CONFIG.shareTrack
   },
   render: h => h(App)
 }).$mount('#app')
