93b756f721cdd97b6de60ccf745c484c014f5594
diandi <diandi@in66.com>
2017-06-16 17:52:48 +0800

    add qiniu compress

--- template/config/index.js
+++ template/config/index.js
@@ -8,7 +8,7 @@ export const build = {
   entryRule: ['entry.js', 'tpl.pug'],
   remUnit: 75,
   imgRegx: {
-    compress: ['src/assets/img/**/**.*'],
+    compress: ['src/assets/img/**/**.*', 'src/assets/qiniu/**/**.*'],
     qiniu: ['src/assets/qiniu/**/**.*']
   },
   bundleAnalyzerReport: process.env.npm_config_report
