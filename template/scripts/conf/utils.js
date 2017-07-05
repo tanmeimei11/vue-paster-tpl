533185c537bbe17b0052bcc8ff9ba0f9e1f7217c
maifei <maifei@in66.com>
2017-07-05 22:45:47 +0800

    modified:   template/scripts/conf/utils.js

--- template/scripts/conf/utils.js
+++ template/scripts/conf/utils.js
@@ -7,6 +7,7 @@ export const iuiAlias = {
   'iScss': 'i-ui/src/scss',
   'iFiters': 'i-ui/src/fiters',
   'iExtends': 'i-ui/src/extends',
+  'iProtocols': 'i-ui/src/iProtocols',
   'iUtil': 'i-ui/src/utils'
 }
 
