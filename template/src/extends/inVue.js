bd6488eaed350378311bc806c2a268769bb6dd4e
maifei <maifei@in66.com>
2017-07-21 16:00:38 +0800

    add cotent-loaded

--- template/src/extends/inVue.js
+++ template/src/extends/inVue.js
@@ -1,34 +1,35 @@
 import Vue from 'vue'
-import { initPlugin } from 'plugins/initPlugin' 
+import { initPlugin } from 'plugins/initPlugin'
 import { iTrack } from 'i-ui'
 import { inpromo } from 'mixins/inPromo'
-import * as config from 'config' 
+import * as config from 'config'
 import injectFetch from 'mocks'
+import 'assets/libs/content-loaded'
 
-window._trackPrefix = config.trackPrefix(location) 
+window._trackPrefix = config.trackPrefix(location)
 
 Vue.$hideGlobalLoading = () => {
   document.getElementById('global-loading').style.display = 'none'
 }
 if (config.hideGlobalLoading === 0) {
-  window.onload = () => {
+  window.contentLoaded(() => {
     Vue.$hideGlobalLoading()
-  }
+  })
 }
 
 class InVue extends Vue {
-  static isMock = process.env.NODE_ENV !== 'production' && config.mock  
+  static isMock = process.env.NODE_ENV !== 'production' && config.mock
   static basePlugins = [
     // directives
-    iTrack, 
-    // 初始化插件 
+    iTrack,
+    // 初始化插件
     initPlugin
   ]
 
   constructor (options) {
-    options.track = config.track 
-    options.hideGlobalLoading = config.hideGlobalLoading 
-    options.share = config.shareMap(location) 
+    options.track = config.track
+    options.hideGlobalLoading = config.hideGlobalLoading
+    options.share = config.shareMap(location)
     options.plugins = options.plugins || []
     InVue.basePlugins.concat(options.plugins).forEach(plugin => Vue.use(plugin))
     super(options)
@@ -39,13 +40,13 @@ class InVue extends Vue {
     if (InVue.isMock) {
       promise = promise.then(() => config.mockMap())
         .then(map => { window.fetch = injectFetch(window.fetch, map) })
-    } 
+    }
     return promise
   }
 
   static batchMixin (mixins) {
     mixins.forEach((mixin) => {
-      InVue.mixin(mixin)  
+      InVue.mixin(mixin)
     })
   }
 }
