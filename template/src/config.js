4f05170cd8dcaf6804864c42c5fb963335832a19
diandi <diandi@in66.com>
2017-08-18 14:47:56 +0800

    add pabei host

--- template/src/config.js
+++ template/src/config.js
@@ -5,6 +5,8 @@
 // export const U_IN_QA = '//qainlove.in66.com'
 // export const U_IN = '//promo.in66.com'
 // export const U_IN_QA = '//qapromo.in66.com'
+// export const U_IN = '//pabei.in66.com'
+// export const U_IN_QA = '//qapabei.in66.com'
 export const U_IN = '//www.in66.com'
 export const U_IN_QA = '//qa.in66.com'
 export const U_IN_APPLINKS = '//m.in66.com/applinks'
