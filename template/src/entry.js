ee625968871b2a172c2785f944b6b52998e7f18e
diandi <diandi@in66.com>
2017-09-20 15:40:26 +0800

    add info

--- template/src/entry.js
+++ template/src/entry.js
@@ -1,4 +1,12 @@
 import app from './units/InDva'
+{{#if_eq analytics "yes"}}
+// 网页监控sdk
+import 'analytics'
+{{/if_eq}}
+{{#if_eq growingIO "yes"}}
+// 活动必须的growingio埋点
+import 'assets/libs/growingio.js'
+{{/if_eq}}
 import { trackApi, awake } from './units/common'
 import toast from './units/toast'
 app.use(toast)
