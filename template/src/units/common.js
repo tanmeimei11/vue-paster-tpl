8863dbe418d7e7a4c5284d03ed55add090d60582
diandi <diandi@in66.com>
2017-09-21 12:49:31 +0800

    fix error

--- template/src/units/common.js
+++ template/src/units/common.js
@@ -2,8 +2,11 @@ import { shareMap, C_IN, U_IN_WXSDK, U_TRACK, QNTokenUrl, apiCommonParam, apiMap
 import { Share, buildtrack, FetchApi, buildUploadQiniu } from 'common-utils'
 export { awake, app, common, is } from 'common-utils'
 
-let share = new Share({sdkUrl: U_IN_WXSDK, trackUrl: U_TRACK})
-export const shareApi = (share.config = shareMap, share.config)
+export const shareApi = () => {
+  let share = new Share({sdkUrl: U_IN_WXSDK, trackUrl: U_TRACK})
+  share.config = shareMap
+  return share.config
+}
 
 export const serverApi = FetchApi(apiCommonParam, apiMap, {
   host: C_IN
